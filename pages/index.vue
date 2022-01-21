f5fe53ebce242bf3ad8fa40ec37acff0766fd371
WendelinGerein <wendelin.gerein@deutschebahn.com>
2022-01-21 16:47:31 +0100

    create first

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Tutorial />
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Tutorial from '@/components/Tutorial.vue'
+@Component({
+  components: {
+    Tutorial
+  }
+})
+export default class PageIndex extends Vue {
+  message: string = 'This is a message'
+}
+</script>
