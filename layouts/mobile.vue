5ec7a98acdd645fa23c5d34afbb1d92fbd56ee44
wendelin <info@webdelin.de>
2022-01-28 18:55:59 +0100

    ts server working

--- layouts/mobile.vue
+++ layouts/mobile.vue
@@ -8,7 +8,6 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component({
-    layout: ({ isMobile }) => isMobile ? 'mobile' : 'default',
   name: 'LayoutMobile'
 })
 export default class LayoutMobile extends Vue {}
